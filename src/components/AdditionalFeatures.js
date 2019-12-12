1b9cf9bf32c00111a9d3fb69f7064b3ea27afc38
CalebAllard <allard.caleb@gmail.com>
2019-12-11 19:02:53 -0500

    need help!

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -5,10 +5,11 @@ const AdditionalFeatures = props => {
   return (
     <div className="content">
       <h4>Additional Features</h4>
+      
       {props.additionalFeatures.length ? (
         <ol type="1">
           {props.additionalFeatures.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature key={item.id} feature={item} addFeature={props.addFeature}/>
           ))}
         </ol>
       ) : (
